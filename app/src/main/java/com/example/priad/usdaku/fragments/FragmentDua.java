11c3722e60b7b0ae92169b6366f6ca6c8151f71b
Dan Bo <Dan Bo>
2017-02-06 08:39:37 +0700

    Penambahan Menu Dua

--- app/src/main/java/com/example/priad/usdaku/fragments/FragmentDua.java
+++ app/src/main/java/com/example/priad/usdaku/fragments/FragmentDua.java
@@ -5,21 +5,25 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.priad.usdaku.R;
+import com.example.priad.usdaku.adapter.RecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class FragmentDua extends Fragment {
+    private RecyclerView rvView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> dataSet;
 
     public FragmentDua() {
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
     }
 
     @Nullable
@@ -28,7 +32,44 @@ public class FragmentDua extends Fragment {
 
         View view = inflater.inflate(R.layout.activity_fragment_dua, container, false);
 
+        dataSet = new ArrayList<>();
+        initDataset();
+
+        rvView = (RecyclerView) view.findViewById(R.id.rv_main);
+        rvView.setHasFixedSize(true);
+
+        /**
+         * Kita menggunakan LinearLayoutManager untuk list standar
+         * yang hanya berisi daftar item
+         * disusun dari atas ke bawah
+         */
+        layoutManager = new LinearLayoutManager(getContext());
+        rvView.setLayoutManager(layoutManager);
+
+        adapter = new RecyclerViewAdapter(dataSet);
+        rvView.setAdapter(adapter);
+
         return view;
+
+    }
+
+    private void initDataset() {
+
+        /**
+         * Tambahkan item ke dataset
+         * dalam prakteknya bisa bermacam2
+         * tidak hanya String seperti di kasus ini
+         */
+        dataSet.add("Karin");
+        dataSet.add("Ingrid");
+        dataSet.add("Helga");
+        dataSet.add("Renate");
+        dataSet.add("Elke");
+        dataSet.add("Ursula");
+        dataSet.add("Erika");
+        dataSet.add("Christa");
+        dataSet.add("Gisela");
+        dataSet.add("Monika");
 
     }
 }
